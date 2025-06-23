714976852eecbe0078eebf492acf4a5289022d0a
filipeped <filipepedoo@gmail.com>
2025-06-23 18:44:09 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.digitalpaisagismo.online"
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,OPTIONS"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type"
+          }
+        ]
+      }
+    ];
+  }
+};
