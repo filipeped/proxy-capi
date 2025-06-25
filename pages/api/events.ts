c13752f52fe6b7239aca11735095aca443c1e784
filipeped <filipepedoo@gmail.com>
2025-06-25 10:27:29 -0300

    Update events.ts

--- pages/api/events.ts
+++ pages/api/events.ts
@@ -4,7 +4,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Origin", "https://www.digitalpaisagismo.online");
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     return res.status(200).end();
   }
 
@@ -12,16 +13,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(405).json({ error: "Method Not Allowed" });
   }
 
+  const FB_PIXEL_ID = "3474069809483558";
+  const FB_TOKEN = process.env.FB_TOKEN;
+  const url = `https://graph.facebook.com/v19.0/${FB_PIXEL_ID}/events?access_token=${FB_TOKEN}`;
+
   try {
-    const response = await fetch("https://graph.facebook.com/v19.0/3474069809483558/events?access_token=EAAQfmxkTTZCcBOwAhY9Qpfqm0gQ1L3ZAhV3EpsZCSC298vN5oaPMwgdsc6ZCWXFRaBzQlyAHfs4HWlUZAefUh50FGw4gn3NIES5iYFZBoumN2HZAMdVvMh1Wtu9OAunizQoYrZAwJUcFSlZCgvg4HMXp1vgSXQDPBiIadqRO4CgxQU0tCYQBN9JMUMrUY8TMRuQZDZD", {
+    const response = await fetch(url, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(req.body)
+      body: JSON.stringify(req.body),
     });
 
     const data = await response.json();
+
+    if (!response.ok) {
+      console.error("Erro da Meta:", data);
+    }
+
     res.status(response.status).json(data);
   } catch (err) {
     console.error("Erro no Proxy CAPI:", err);
