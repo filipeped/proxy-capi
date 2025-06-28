feb4acaa78f16c540b57edc7c5a2dfcd8314a457
filipeped <filipepedoo@gmail.com>
2025-06-28 02:00:59 -0300

    Update events.ts

--- pages/api/events.ts
+++ pages/api/events.ts
@@ -16,19 +16,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     const payload = {
       ...req.body,
-      client_ip_address: req.headers["x-forwarded-for"] || undefined,
+      client_ip_address: req.headers["x-forwarded-for"] || undefined
     };
 
-    const response = await fetch(
-      "https://graph.facebook.com/v19.0/3474069809483558/events?access_token=EAAQfmxkTTZCcBOwAhY9Qpfqm0gQ1L3ZAhV3EpsZCSC298vN5oaPMwgdsc6ZCWXFRaBzQlyAHfs4HWlUZAefUh50FGw4gn3NIES5iYFZBoumN2HZAMdVvMh1Wtu9OAunizQoYrZAwJUcFSlZCgvg4HMXp1vgSXQDPBiIadqRO4CgxQU0tCYQBN9JMUMrUY8TMRuQZDZD",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      }
-    );
+    const response = await fetch("https://graph.facebook.com/v19.0/3474069809483558/events?access_token=EAAQfmxkTTZCcBOwAhY9Qpfqm0gQ1L3ZAhV3EpsZCSC298vN5oaPMwgdsc6ZCWXFRaBzQlyAHfs4HWlUZAefUh50FGw4gn3NIES5iYFZBoumN2HZAMdVvMh1Wtu9OAunizQoYrZAwJUcFSlZCgvg4HMXp1vgSXQDPBiIadqRO4CgxQU0tCYQBN9JMUMrUY8TMRuQZDZD", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    });
 
     const data = await response.json();
     res.status(response.status).json(data);
